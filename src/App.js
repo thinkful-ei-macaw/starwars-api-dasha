4c876bcff27c9fd00443dd3838f9019fe4b6a808
Dasha Akhten <dasha@Dashas-iMac.attlocal.net>
2020-03-13 09:32:02 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>StarWars App</h1>
+        <img
+          id="logo"
+          src="/images/1280px-Star_Wars_Logo.svg.png"
+          alt="Star Wars logo"
+        />
+
+        <p>I dont know anything about StarWars</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
